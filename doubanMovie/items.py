d75cae38e91ca8b31cc1424cf705347f5b136a71
jianrongzeng <2471788627@qq.com>
2019-01-06 00:33:22 +0800

    能把后续链接抓取并添加到待爬取队列（通过yield关键字或者return request）

--- doubanMovie/items.py
+++ doubanMovie/items.py
@@ -12,3 +12,4 @@ class DoubanmovieItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     title = scrapy.Field()
+    url = scrapy.Field()
--- doubanMovie/spiders/doubanmovieSpider.py
+++ doubanMovie/spiders/doubanmovieSpider.py
@@ -7,7 +7,7 @@ from doubanMovie.items import DoubanmovieItem
 class DoubanmovieSpider(scrapy.Spider):
     name = 'doubanmovie'
     allowed_domains = ['movie.douban.com']
-    start_urls = ['https://movie.douban.com/']
+    start_urls = ['https://movie.douban.com/people/85965842/collect']
 
     custom_settings = {
         "DEFAULT_REQUEST_HEADERS": {
@@ -16,21 +16,22 @@ class DoubanmovieSpider(scrapy.Spider):
     }
 
     def parse(self, response):
-        # filename = "indexdouban.html"
-        # open(filename, "w").write(response)
-        # print(response.body.decode('utf-8'))
         items = []
-        for div in response.xpath("//a[@class='item']"):
+        for index, div in enumerate(response.xpath("//div[@class='item']")):
             item = DoubanmovieItem()
-            title = div.xpath("//@href").extract()
-
+            title = div.xpath(".//em/text()").extract()[0]
+            url = div.xpath(".//li[@class='title']//a/@href").extract()[0]
+            yield scrapy.Request(url, callback=self.parse_detail)
+            # scrapy.Request()
             item["title"] = title
-
-            items.append(item)
+            # item["url"] = url
+            # print("123:" + response.url)
+            # items.append(item)
+        next_page_href = response.xpath("//link[@rel='next']/@href").extract()[0]
+        print(response.urljoin(next_page_href))
+        next_page_url = response.urljoin(next_page_href)
+        yield scrapy.Request(next_page_url, callback=self.parse)
         return items
 
-
-
-
-
-
+    def parse_detail(self, response):
+        return 0
